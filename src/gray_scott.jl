24db59ecfabdcb611a2d7f7a874cccef740e02e4
Justin Browning <jtbrowning1996@icloud.com>
2023-06-18 15:52:43 -0500

    Initializing files and folders for this simulation project.

---
+++ src/gray_scott.jl
@@ -0,0 +1,21 @@
+function init(n)
+    u = ones(n+2, n+2)
+    v = zeros(n+2, n+2)
+
+
+end
+
+function periodic_bc(u)
+    return u
+end
+
+function laplacian(u)
+    return u
+end
+
+function gray_scott()
+    return 0
+end
+
+# VISUALIZATION
+
