04b474469016b13a780e8313f3ce0038c04e2376
Justin Browning <jtbrowning1996@icloud.com>
2023-06-19 13:57:47 -0500

    implement functions for the Gray-Scott model and logging.

--- src/gray_scott.jl
+++ src/gray_scott.jl
@@ -16,21 +16,83 @@ function init(n)
     return u, v;
 end
 
+function log(u, v)
+    #Logging:
+    println("Successful Operation")
+    println("====================================================================================")
+    show(stdout, "text/plain", u)
+    print('\n')
+    show(stdout, "text/plain", v)
+    println()
+    println("====================================================================================")
+    println()
+end
+
 function periodic_bc(u)
-    u[0,:] = u[-2,:];
-    u[-1,:] = u[1,:];
-    u[:,0] = u[:,-2];
-    u[:,-1] = u[:,1];
+    # Periodic Boundary Conditions
+    u[1,:] = u[end-1,:]
+    u[end,:] = u[2,:]
+    u[:,1] = u[:,end-1]
+    u[:,end] = u[:,2]
 end
 
 function laplacian(u)
     # Second Order Finite Difference 
-    laplacian = u[]
+    laplacian = zeros(10,10);
+
+    for i in 2:11, j in 2:11
+        laplacian[i-1,j-1] = u[i+1,j] + u[i-1,j] + u[i,j+1] + u[i,j-1] - 4*u[i,j]
+    end
+
     return laplacian;
 end
 
-function gray_scott(U, V, )
-    return 0
+function gray_scott(U, V, Du, Dv, f, k)
+    #=
+    U - 'Chemical A' array 
+    V - 'Chemical B' array
+    Du - Diffusion Rate of A
+    Dv - Diffusion Rate of B
+    f - Feed Rate of A
+    k - Kill Rate of B
+
+    The formulas are given as follows:
+
+    ∂u/∂t = Du∇²u - uv² + f(1-u)
+    ∂v/∂t = Dv∇²v + uv² - (f+k)v
+
+    where ∇² is the Laplacian operator, numerically approximated using 
+    the second finite difference.
+    =# 
+
+    u, v = U[2:end-1, 2:end-1], V[2:end-1, 2:end-1];
+    print("(u, v): Import of U and V || ")
+    log(u, v);
+
+    Lu = laplacian(U);
+    Lv = laplacian(V);
+
+    print("(Lu, Lv): Laplacian of U and V || ")
+    log(Lu, Lv);
+
+    uv² = u.*v.^2;
+    u += Du*Lu - uv² .+ f*(1-u);
+    v += Dv*Lv + uv² .- (f+k)*v;
+
+    print("(u, v): After operations are applied to u and v || ")
+    log(u, v);
+    
+    print('\n')
+    print("(U, V): After operations are applied to U and V || ")
+    log(U, V);
+
+    periodic_bc(U);
+    periodic_bc(V);
+
+    print("(U, V): After periodic_bc is applied to U and V || ")
+    log(U, V);
+
+    return U, V;
 end
 
 # VISUALIZATION
--- src/main.jl
+++ src/main.jl
@@ -1,10 +1,25 @@
 include("gray_scott.jl")
 
 function main()
-    u, v = init(10);
-    show(stdout, "text/plain", u)
-    print('\n')
-    show(stdout, "text/plain", v)
+    f = 0.0545;
+    k = 0.6200;
+    Du = .1;
+    Dv = .05;
+    n = 10;
+
+    U, V = init(n);
+
+    
+    print("Initialization Successful || ")
+    log(U,V);
+
+    for t in 1:40
+
+        print("TIMESTEP: ", t, " || ")
+        log(U,V);
+
+        U, V = gray_scott(U, V, Du, Dv, f, k);
+    end
 
 end
 
